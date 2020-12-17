568145f239d4ce8e224e5be2869f3a96abd61daa
fantasyup <fantasy99483@gmail.com>
2020-12-17 22:35:36 +1000

    fix derivation path, specify file path

--- priv-validator-key-gen/main.go
+++ priv-validator-key-gen/main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 
 	"github.com/cosmos/go-bip39"
-	"github.com/tendermint/tendermint/libs/tempfile"
 	"github.com/tendermint/tendermint/privval"
 
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/tendermint/tendermint/crypto/ed25519"
 	tmjson "github.com/tendermint/tendermint/libs/json"
 )
 
-func save(pvKey privval.FilePVKey) {
-}
-
 func main() {
 	mnemonic := flag.String("mnemonic", "swap exercise equip shoot mad inside floor wheel loan visual stereo build frozen always bulb naive subway foster marine erosion shuffle flee action there", "a string")
-	path := flag.String("output", "./", "a string")
+	path := flag.String("output", "./priv_validator_key.json", "a string")
 	flag.Parse()
 
-	privKey := ed25519.GenPrivKeyFromSecret(bip39.NewSeed(*mnemonic, ""))
+	master, ch := hd.ComputeMastersFromSeed(bip39.NewSeed(*mnemonic, ""))
+	priv, err := hd.DerivePrivateKeyForPath(master, ch, "44'/118'/0'/0/0")
+
+	privKey := ed25519.GenPrivKeyFromSecret(priv)
 	filepvkey := privval.FilePVKey{
 		Address: privKey.PubKey().Address(),
 		PubKey:  privKey.PubKey(),
@@ -31,7 +32,7 @@ func main() {
 		panic(err)
 	}
 
-	err = tempfile.WriteFileAtomic(*path+"/priv_validator_key.json", jsonBytes, 0600)
+	err = ioutil.WriteFile(*path, jsonBytes, 0644)
 	if err != nil {
 		panic(err)
 	}
