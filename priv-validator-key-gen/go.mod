ec7a66ff2271c9a151b79b746b855334660676c9
fantasyup <fantasy99483@gmail.com>
2020-12-22 11:52:21 +1000

    add node-key, node-id

--- priv-validator-key-gen/go.mod
+++ priv-validator-key-gen/go.mod
@@ -6,6 +6,4 @@ require (
 	github.com/cosmos/cosmos-sdk v0.34.4-0.20200917134338-9cb27fb171aa
 	github.com/cosmos/go-bip39 v0.0.0-20180819234021-555e2067c45d
 	github.com/tendermint/tendermint v0.34.0
-	github.com/tyler-smith/go-bip39 v1.1.0
-	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9
 )
--- priv-validator-key-gen/main.go
+++ priv-validator-key-gen/main.go
@@ -2,37 +2,60 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 
-	"github.com/cosmos/go-bip39"
-	"github.com/tendermint/tendermint/privval"
-
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/go-bip39"
 	"github.com/tendermint/tendermint/crypto/ed25519"
-	tmjson "github.com/tendermint/tendermint/libs/json"
+	"github.com/tendermint/tendermint/p2p"
+	"github.com/tendermint/tendermint/privval"
 )
 
 func main() {
-	mnemonic := flag.String("mnemonic", "swap exercise equip shoot mad inside floor wheel loan visual stereo build frozen always bulb naive subway foster marine erosion shuffle flee action there", "a string")
-	path := flag.String("output", "./priv_validator_key.json", "a string")
+	mnemonic := flag.String("mnemonic", "", "a string")
+	valkey := flag.String("valkey", "", "a string")
+	nodekey := flag.String("nodekey", "", "a string")
+	keyid := flag.String("keyid", "", "a string")
 	flag.Parse()
+
+	if len(*mnemonic) == 0 {
+		fmt.Println("mnemonic not set!")
+		return
+	}
+	if len(*valkey) == 0 {
+		fmt.Println("valkey not set!")
+		return
+	}
+	if len(*nodekey) == 0 {
+		fmt.Println("nodekey not set!")
+		return
+	}
+	if len(*keyid) == 0 {
+		fmt.Println("keyid not set!")
+		return
+	}
 
 	master, ch := hd.ComputeMastersFromSeed(bip39.NewSeed(*mnemonic, ""))
 	priv, err := hd.DerivePrivateKeyForPath(master, ch, "44'/118'/0'/0/0")
-
-	privKey := ed25519.GenPrivKeyFromSecret(priv)
-	filepvkey := privval.FilePVKey{
-		Address: privKey.PubKey().Address(),
-		PubKey:  privKey.PubKey(),
-		PrivKey: privKey,
-	}
-
-	jsonBytes, err := tmjson.MarshalIndent(filepvkey, "", "  ")
 	if err != nil {
 		panic(err)
 	}
 
-	err = ioutil.WriteFile(*path, jsonBytes, 0644)
+	privKey := ed25519.GenPrivKeyFromSecret(priv)
+	filepvkey := privval.NewFilePV(privKey, *valkey, "").Key
+	filepvkey.Save()
+
+	filenodekey := p2p.NodeKey{
+		PrivKey: privKey,
+	}
+
+	err = filenodekey.SaveAs(*nodekey)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(*keyid, []byte(filenodekey.ID()), 0644)
 	if err != nil {
 		panic(err)
 	}
