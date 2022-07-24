596c53e6eca18660b2a16d59e58861c5f40b15ce
Asmodat <asmodat@gmail.com>
2022-07-24 11:13:48 +0000

    syntax

--- bip39gen/scripts/test.sh
+++ bip39gen/scripts/test.sh
@@ -12,6 +12,6 @@ ENTROPY=$(sha256 "daring collect artist first six arena brown design park syrup 
 MNEMONIC_TEST_1=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY")
 MNEMONIC_TEST_2=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY")
 
-[ "$MNEMONIC_TEST_1" != "$MNEMONIC_TEST_2" ] && \
+[ "$MNEMONIC_TEST_1" == "$MNEMONIC_TEST_2" ] && \
  echoErr "ERROR: When user entropy is provided expected mnemonic to change every time, in the process of being mixed with computer provided entropy" && exit 1 || echoInfo "INFO: Test 1 passed"
 
