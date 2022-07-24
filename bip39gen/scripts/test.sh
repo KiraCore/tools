5e8520ba7ea41fc0c2f8d9d2fb92964759900350
Asmodat <asmodat@gmail.com>
2022-07-24 11:05:37 +0000

    syntax

--- bip39gen/scripts/test.sh
+++ bip39gen/scripts/test.sh
@@ -8,11 +8,11 @@ set -x
 . ./bip39/scripts/build.sh
 . ./scripts/build.sh
 
-cd $GOBIN
+BIN_bip39gen="${GOBIN}/bip39gen"
 
 ENTROPY=$(sha256 "daring collect artist first six arena brown design park syrup jump pluck")
-MNEMONIC_TEST_1=$(bip39gen mnemonic -l 12 -e "$ENTROPY")
-MNEMONIC_TEST_2=$(bip39gen mnemonic -l 12 -e "$ENTROPY")
+MNEMONIC_TEST_1=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY")
+MNEMONIC_TEST_2=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY")
 
 [ "$MNEMONIC_TEST_1" != "$MNEMONIC_TEST_2" ] && \
  echoErr "ERROR: When user entropy is provided expected mnemonic to change every time, in the process of being mixed with computer provided entropy" && exit 1 || echoInfo "INFO: Test 1 passed"
