13d4d920a78132a40cdc9f4df929da7f53291910
Asmodat <asmodat@gmail.com>
2023-01-11 22:57:10 +0000

    set correct version

--- bash-utils/bash-utils.sh
+++ bash-utils/bash-utils.sh
@@ -24,7 +24,7 @@ function bashUtilsVersion() {
 # this is default installation script for utils
 # ./bash-utils.sh bashUtilsSetup "/var/kiraglob"
 function bashUtilsSetup() {
-    local BASH_UTILS_VERSION="v0.3.31"
+    local BASH_UTILS_VERSION="v0.3.21"
     local COSIGN_VERSION="v1.13.1"
     if [ "$1" == "version" ] ; then
         echo "$BASH_UTILS_VERSION"
