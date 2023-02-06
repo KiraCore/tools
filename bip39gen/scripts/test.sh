abc19492ecc9f04de7aed4c67cf231e538b478bf
mrlutik <yaievgeniy@gmail.com>
2023-02-06 15:45:59 +0100

    bip39gen/test added --hex flag

--- bip39gen/scripts/test.sh
+++ bip39gen/scripts/test.sh
@@ -16,8 +16,8 @@ go test -v -cover ./...
 BIN_bip39gen="${GOBIN}/bip39gen"
 
 ENTROPY="$(echo "daring collect artist first six arena brown design park syrup jump pluck" | sha256)"
-MNEMONIC_TEST_1=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY" -h)
-MNEMONIC_TEST_2=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY" -h)
+MNEMONIC_TEST_1=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY" --hex)
+MNEMONIC_TEST_2=$($BIN_bip39gen mnemonic -l 12 -e "$ENTROPY" --hex)
 
 [ "$MNEMONIC_TEST_1" == "$MNEMONIC_TEST_2" ] && \
  echoErr "ERROR: When user entropy is provided expected mnemonic to change every time, in the process of being mixed with computer provided entropy" && exit 1 || echoInfo "INFO: Test 1 passed"
