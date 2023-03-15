47ae7a0344a7b2b09a69eed43ad8f11b20879810
mrlutik <yaievgeniy@gmail.com>
2023-03-15 22:38:21 +0100

    f i x

--- validator-key-gen/main.go
+++ validator-key-gen/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/tendermint/tendermint/privval"
 )
 
-const PrivValidatorKeyGenVersion = "v0.3.23-rc.04"
+const PrivValidatorKeyGenVersion = "v0.3.23-rc.05"
 
 type Prefix struct {
 	fullPath             *hd.BIP44Params
--- validator-key-gen/scripts/test.sh
+++ validator-key-gen/scripts/test.sh
@@ -1,13 +1,8 @@
-#!/usr/bin/env bash
-
-# Set mnemonic variable
+#!/usr/bin/bash
 mnemonic="\"over where supreme taste warrior morning perfect memory glove stereo taste trip sheriff fringe weather finger segment under arrange gain warrior olympic urge vacant\""
-
-# Testing address generation with predetermined mnemonic against hard coded results
 Output=("kira103luqf09g5juctmvrmgnw5gmn2mhpelqhcsy84" "kiravaloper103luqf09g5juctmvrmgnw5gmn2mhpelqy7v8le" "kiravalcons103luqf09g5juctmvrmgnw5gmn2mhpelqsdlmnc")
 AdrType=("Account address" "Validator address" "Consensus address")
 Input=("go run . --mnemonic=${mnemonic} --accadr" "go run . --mnemonic=${mnemonic} --valadr" "go run . --mnemonic=${mnemonic} --consadr")
-
 testadr(){
     echo "Testing address formation:"
     for i in $(seq 0 $((${#Input[@]} - 1))); do
@@ -24,12 +19,9 @@ testadr(){
     done
     return 0
 }
-
-# Checking md5 checksum of created files against hard coded results
 md5=("8a100779d27e5ae2098498674df32f8b" "d14df3851190d360953989e296db3cf3" "7ab595fe3d53672ac918a351bcaa10b5")
 files=("./valkey" "./nodekey" "./keyid")
 cmd=("go run . --mnemonic=${mnemonic} --valkey=${files[0]}" "go run . --mnemonic=${mnemonic} --nodekey=${files[1]}" "go run . --mnemonic=${mnemonic} --keyid=${files[2]}")
-
 testmd5(){
     echo "Checking files md5 checksum:"
     for i in $(seq 0 $((${#cmd[@]} - 1))); do
@@ -47,8 +39,6 @@ testmd5(){
     done
     return 0
 }
-
-# Deleting files created by testmd5 function
 clean(){
     echo "Deleting files:"
     for f in ${files[@]}; do 
@@ -57,9 +47,7 @@ clean(){
     done
     return 0
 }
-
 tests=(testadr testmd5 clean)
-
 test() {
     errs=()
     for test in "${tests[@]}"; do
