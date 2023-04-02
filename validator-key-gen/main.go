c92ad1922580690cac9cbc337e8c1a300128ccee
mrlutik <yaievgeniy@gmail.com>
2023-04-02 23:36:06 +0200

    Change ver v0.3.42 -> v0.3.43

--- validator-key-gen/main.go
+++ validator-key-gen/main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/tendermint/tendermint/privval"
 )
 
-const PrivValidatorKeyGenVersion = "v0.3.39"
+const PrivValidatorKeyGenVersion = "v0.3.43"
 
 type Prefix struct {
 	fullPath             *hd.BIP44Params
