2ea7b7f5a1e43b6a44280fc96f355174a2ffe957
mrlutik <yaievgeniy@gmail.com>
2023-04-02 11:48:40 +0200

    Add comments, docstring and description
    Improve readability of cli.unpin func

--- ipfs-api/pkg/cli/unpin.go
+++ ipfs-api/pkg/cli/unpin.go
@@ -16,24 +16,39 @@ var unpinCommand = &cobra.Command{
 	RunE:  unpin,
 }
 
+// unpin is a function that handles the unpin command with the provided arguments.
+// This function unpin content from IPFS using the Pinata API.
+//
+// cmd: The cobra command instance.
+// args: The arguments passed to the unpin command.
+//
+// Returns an error if the unpin operation fails or the arguments are empty.
 func unpin(cmd *cobra.Command, args []string) error {
+	// Check if the arguments are empty.
 	if len(args) == 0 {
-		log.Error("pinned: empty arg")
+		log.Error("unpin: empty arg")
 		return errors.New("args can't be empty")
 	}
+
+	// Get the keys and initialize the PinataApi instance.
 	keys, err := pnt.GrabKey(key)
 	if err != nil {
 		log.Error("failed to process keys")
 		return err
 	}
 
+	// Set up the PinataApi instance with the obtained keys and the provided argument.
 	p := pnt.PinataApi{}
 	p.SetKeys(keys)
 	p.SetData(args[0])
+
+	// Unpin the content using the Pinata API.
 	if err := p.Unpin(args[0]); err != nil {
 		log.Error("unable to unpin: %v", err)
 		return err
 	}
+
+	// Output the result of the unpin operation.
 	err = p.OutputUnpinJson()
 	if err != nil {
 		log.Error("failed to print results")
