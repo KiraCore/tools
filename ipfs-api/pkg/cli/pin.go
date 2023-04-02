a600f66adeb37941e348452b87b86b4f0c48a28d
mrlutik <yaievgeniy@gmail.com>
2023-04-02 11:43:25 +0200

    Refactor PinCmd
    Add docstrings and comments, separate logic for force and ovewrite flags, for result output. Add descriptive names to args. Improve overall readability

--- ipfs-api/pkg/cli/pin.go
+++ ipfs-api/pkg/cli/pin.go
@@ -28,12 +28,21 @@ var pinCommand = &cobra.Command{
 	RunE:  pinCmd,
 }
 
+// pinCmd is a function that handles the pin command with the provided flags and arguments.
+// This function handles pinning content to IPFS using the Pinata API and managing metadata.
+//
+// cmd: The cobra command instance.
+// args: The arguments passed to the pin command.
+//
+// Returns an error if any operation fails.
 func pinCmd(cmd *cobra.Command, args []string) error {
+	// Check if both overwrite and force flags are set, which is a conflict.
 	if overwrite && force {
 		log.Error("pinCmd: conflict: only one flag can be set")
 		return errors.New("conflict: only one flag can be set")
 	}
 
+	// Get the keys and initialize the PinataApi instance.
 	keys, err := pnt.GrabKey(key)
 	if err != nil {
 		log.Error("grabKey: failed to get keys: %v", err)
@@ -42,104 +51,116 @@ func pinCmd(cmd *cobra.Command, args []string) error {
 	p := pnt.PinataApi{}
 	p.SetKeys(keys)
 
+	// Set metadata if provided.
+	if len(meta) > 0 {
+		m, err := pnt.StrToMeta(meta)
+		if err != nil {
+			return err
+		}
+		p.SetMetaData(m)
+	}
+
+	// Assign args[0] and args[1] to descriptive variable names
+	content := args[0]
+	name := args[1]
+
 	switch len(args) {
 	case 1:
 		if force {
 			log.Error("pinCmd: can't force if metadata is not provided")
 			return errors.New("can't force if metadata is not provided")
 		}
-		if len(meta) > 0 {
-			m, err := pnt.StrToMeta(meta)
-			if err != nil {
-				return err
-			}
-			p.SetMetaData(m)
-		}
 
-		err := p.Pin(args[0])
+		// Pin the content and output the result.
+		err := pinAndOutputResult(&p, content)
 		if err != nil {
-			log.Error("pin failed %v", err)
 			return err
-		}
-		if err := p.OutputPinJson(); err != nil {
-			log.Error("failed to print results: %v", err)
-			os.Exit(1)
 		}
 
 	case 2:
-		switch force {
-		case false:
-			switch overwrite {
-			case false:
-				if err := p.SetMetaName(args[1]); err != nil {
-					log.Error("failed to add metadata %v", err)
-					return err
-				}
-				if len(meta) > 0 {
-					m, err := pnt.StrToMeta(meta)
-					if err != nil {
-						return err
-					}
-					p.SetMetaData(m)
-				}
-				err := p.Pin(args[0])
-				if err != nil {
-					log.Error("pin failed %v", err)
-					return err
-				}
-				if err := p.OutputPinJson(); err != nil {
-					log.Error("failed to print results: %v", err)
-					os.Exit(1)
-				}
-			case true:
-				if err := p.SetMetaName(args[1]); err != nil {
-					p.Unpin(args[1])
-				}
-				if err := p.SetMetaName(args[1]); err != nil {
-					log.Error("failed to add metadata %v", err)
-					return err
-				}
-				if len(meta) > 0 {
-					m, err := pnt.StrToMeta(meta)
-					if err != nil {
-						return err
-					}
-					p.SetMetaData(m)
-				}
-
-				err := p.Pin(args[0])
-				if err != nil {
-					log.Error("pin failed %v", err)
-					return err
-				}
-
-			}
-		case true:
-			if len(meta) > 0 {
-				m, err := pnt.StrToMeta(meta)
-				if err != nil {
-					return err
-				}
-				p.SetMetaData(m)
-			}
-			err := p.Pin(args[0])
-			if err != nil {
-				log.Error("pin failed %v", err)
-				return err
-			}
-			o, err := p.OutputPinJsonObj()
-			if err != nil {
-				return err
-			}
-			if o.Duplicate {
-				err := p.SetMeta(o.IpfsHash, args[1])
-				if err != nil {
-					return err
-				}
-			}
-
+		// Set the name metadata.
+		if err := p.SetMetaName(name); err != nil {
+			log.Error("failed to add metadata %v", err)
+			return err
 		}
 
+		switch {
+		case force:
+			// Pin the content, check for duplicates, and update metadata if necessary.
+			err = pinAndForce(&p, content, name)
+
+		case overwrite:
+			// Unpin previous content, pin the new content, and update metadata.
+			err = pinAndOverwrite(&p, content, name)
+
+		default:
+			// Pin the content and output the result.
+			err = pinAndOutputResult(&p, content)
+		}
 	}
+
+	return err
+}
+
+// pinAndOutputResult pins the content and outputs the result.
+func pinAndOutputResult(p *pnt.PinataApi, content string) error {
+	err := p.Pin(content)
+	if err != nil {
+		log.Error("pin failed %v", err)
+		return err
+	}
+	if err := p.OutputPinJson(); err != nil {
+		log.Error("failed to print results: %v", err)
+		os.Exit(1)
+	}
+	return nil
+}
+
+// pinAndForce pins the content, checks for duplicates, and updates metadata if necessary.
+func pinAndForce(p *pnt.PinataApi, content string, name string) error {
+	err := p.Pin(content)
+	if err != nil {
+		log.Error("pin failed %v", err)
+		return err
+	}
+	o, err := p.OutputPinJsonObj()
+	if err != nil {
+		return err
+	}
+	if o.Duplicate {
+		err := p.SetMeta(o.IpfsHash, name)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// pinAndOverwrite unpins previous content, pins the new content, and updates metadata.
+func pinAndOverwrite(p *pnt.PinataApi, content string, name string) error {
+	// Unpin the previous content.
+	if err := p.Unpin(name); err != nil {
+		log.Error("failed to unpin previous content: %v", err)
+		return err
+	}
+
+	// Pin the new content.
+	err := p.Pin(content)
+	if err != nil {
+		log.Error("pin failed %v", err)
+		return err
+	}
+
+	// Update metadata.
+	o, err := p.OutputPinJsonObj()
+	if err != nil {
+		return err
+	}
+	err = p.SetMeta(o.IpfsHash, name)
+	if err != nil {
+		log.Error("failed to update metadata: %v", err)
+		return err
+	}
+
 	return nil
 }
