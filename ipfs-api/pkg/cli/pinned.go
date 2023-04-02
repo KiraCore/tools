a6ce459b60763fdd349d47628a1142473840a551
mrlutik <yaievgeniy@gmail.com>
2023-04-02 12:03:42 +0200

    Add comments, docstring and description
    Improve readability of cli.pinned func

--- ipfs-api/pkg/cli/pinned.go
+++ ipfs-api/pkg/cli/pinned.go
@@ -17,22 +17,37 @@ var pinnedCommand = &cobra.Command{
 	RunE:  pinned,
 }
 
+// pinned retrieves pinned content by its IPFS hash or metadata name using the Pinata API.
+// This function returns an error if the pinned content cannot be retrieved or if the output operation fails.
+//
+// cmd: The command instance received by the function.
+// args: A slice of string arguments provided to the command.
+//
+// Returns an error if any operation fails.
 func pinned(cmd *cobra.Command, args []string) error {
+	// Check if the arguments are empty.
 	if len(args) == 0 {
 		log.Error("pinned: empty arg")
 		return errors.New("args can't be empty")
 	}
+
+	// Grab the API key.
 	keys, err := pnt.GrabKey(key)
 	if err != nil {
 		return err
 	}
 
+	// Initialize a PinataApi instance and set the API keys.
 	p := pnt.PinataApi{}
 	p.SetKeys(keys)
+
+	// Retrieve the pinned content by IPFS hash or metadata name.
 	if err := p.Pinned(args[0]); err != nil {
-		log.Error("Cant't pin this")
+		log.Error("Can't pin this")
 		return err
 	}
+
+	// Output the pinned content as JSON.
 	err = p.OutputPinnedJson()
 	if err != nil {
 		return err
