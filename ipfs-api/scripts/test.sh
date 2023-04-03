1c0ff9d000624772df5b1548cc4f7e1c21051777
mrlutik <yaievgeniy@gmail.com>
2023-04-03 14:52:20 +0200

    Add bu before func call

--- ipfs-api/scripts/test.sh
+++ ipfs-api/scripts/test.sh
@@ -1,38 +1,38 @@
 set -e
 set -x
 
-echoInfo "Starting unit tests..."
-go test ./... -vet=off -v || echoInfo "IPFS-API test finished successfully"
+bu echoInfo "Starting unit tests..."
+go test ./... -vet=off -v || bu echoInfo "IPFS-API test finished successfully"
 
-echoInfo "Starting integration tests..."
+bu echoInfo "Starting integration tests..."
 
 ROOT_DIR=$(pwd)
 MAIN_DIR=$ROOT_DIR/cmd/ipfs-api/main.go
 ENTRY_DIR=$ROOT_DIR/test_dir
 SECOND_DIR=$ENTRY_DIR/test_dir1
 
-echoInfo "Creating directory tree..."
-mkdir -p $ENTRY_DIR || echoError "Failed to create directory $ENTRY_DIR"
-mkdir -p $SECOND_DIR || echoError "Failed to create directory $SECOND_DIR"
+bu echoInfo "Creating directory tree..."
+mkdir -p $ENTRY_DIR || bu echoError "Failed to create directory $ENTRY_DIR"
+mkdir -p $SECOND_DIR || bu echoError "Failed to create directory $SECOND_DIR"
 
 
-echoInfo "Populating directory tree with files..."
+bu echoInfo "Populating directory tree with files..."
 # Populate dir with files L1
 set +x 
 for i in {1..5}; 
 do
-    echo "file$i">"$ENTRY_DIR/file$i.txt" || echoError "Failed to create file$i.txt"
+    echo "file$i">"$ENTRY_DIR/file$i.txt" || bu echoError "Failed to create file$i.txt"
 done
 
 # Populate dir with files L2
 for i in {6..10};
 do
-    echo "file$i">"$SECOND_DIR/file$i.txt" || echoError "Failed to create file$i.txt"
+    echo "file$i">"$SECOND_DIR/file$i.txt" || bu echoError "Failed to create file$i.txt"
 done
 
 
 function dagExportTest(){
-    echoNInfo "[    ] dagExportTest"
+    bu echoNInfo "[    ] dagExportTest"
 
     local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
     local GOT=$(go run $MAIN_DIR dag $ENTRY_DIR --export)
@@ -47,7 +47,7 @@ function dagExportTest(){
 }
 
 function pinTest(){
-    echoNInfo "[    ] pinTest"
+    bu echoNInfo "[    ] pinTest"
     local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
     local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR --key="$PINATA_API_JWT_TEST" | jq -r .hash)
 
@@ -62,7 +62,7 @@ function pinTest(){
 }
 
 function deleteByHashTest(){
-    echoNInfo "[    ] deleteByHashTest"
+    bu echoNInfo "[    ] deleteByHashTest"
     
     local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
     local GOT=$(go run $MAIN_DIR delete $WANT --key="$PINATA_API_JWT_TEST" | jq .success)
@@ -77,7 +77,7 @@ function deleteByHashTest(){
 }
 
 function pinWithMetaTest(){
-    echoNInfo "[    ] pinWithMetaTest"
+    bu echoNInfo "[    ] pinWithMetaTest"
 
     local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
     local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR meta --key="$PINATA_API_JWT_TEST" | jq -r .hash)
@@ -93,7 +93,7 @@ function pinWithMetaTest(){
 }
 
 function pinWithMetaForceTest(){
-    echoNInfo "[    ] pinWithMetaForceTest"
+    bu echoNInfo "[    ] pinWithMetaForceTest"
 
     local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
     local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR foobar --key="$PINATA_API_JWT_TEST" | jq -r .hash)
@@ -108,7 +108,7 @@ function pinWithMetaForceTest(){
 
 }
 function pinWithMetaOverwriteTest(){
-    echoNInfo "[    ] pinWithMetaOverwriteTest"
+    bu echoNInfo "[    ] pinWithMetaOverwriteTest"
 
     local WANT="OK"
     local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR foobar --overwrite --key="$PINATA_API_JWT_TEST")
@@ -123,7 +123,7 @@ function pinWithMetaOverwriteTest(){
 }
 
 function deleteByMetaTest(){
-    echoNInfo "[PASS] deleteByMetaTest"
+    bu echoNInfo "[PASS] deleteByMetaTest"
 
     local WANT=true
     local GOT=$(go run $MAIN_DIR delete meta --key="$PINATA_API_JWT_TEST" | jq .success)
@@ -137,7 +137,7 @@ function deleteByMetaTest(){
 }
 
 function deleteByMetaOverwriteTest(){
-    echoNInfo "[PASS] deleteByMetaOverwriteTest"
+    bu echoNInfo "[PASS] deleteByMetaOverwriteTest"
 
     local WANT=true
     local GOT=$(go run $MAIN_DIR delete foobar --key="$PINATA_API_JWT_TEST" | jq .success)
@@ -151,13 +151,13 @@ function deleteByMetaOverwriteTest(){
     fi
 }
 
-echoInfo "Starting tests..."
+bu echoInfo "Starting tests..."
 
 TESTS=(dagExportTest pinTest deleteByHashTest pinWithMetaTest deleteByMetaTest pinWithMetaTest pinWithMetaOverwriteTest deleteByMetaOverwriteTest pinWithMetaTest pinWithMetaForceTest deleteByMetaOverwriteTest)
 for TEST in "${TESTS[@]}"; do
     $TEST
 done
-echoInfo "All tests finished. Cleaning up the environment..."
+bu echoInfo "All tests finished. Cleaning up the environment..."
 set -x
-rm -rf $ENTRY_DIR || echoError "Failed to clean up an the environment"
-echoInfo "All done..."
+rm -rf $ENTRY_DIR || bu echoError "Failed to clean up an the environment"
+bu echoInfo "All done..."
