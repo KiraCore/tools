35a5ab65ccffd123c286e78d3ad45e9068b61be9
mrlutik <yaievgeniy@gmail.com>
2023-04-04 23:03:52 +0200

    Refactor tests

--- ipfs-api/scripts/test.sh
+++ ipfs-api/scripts/test.sh
@@ -13,6 +13,10 @@ MAIN_DIR=$ROOT_DIR/cmd/ipfs-api/main.go
 ENTRY_DIR=$ROOT_DIR/test_dir
 SECOND_DIR=$ENTRY_DIR/test_dir1
 
+CID="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
+META="meta"
+META_FORCE="foobar"
+
 bu echoInfo "Creating directory tree..."
 mkdir -p $ENTRY_DIR || bu echoError "Failed to create directory $ENTRY_DIR"
 mkdir -p $SECOND_DIR || bu echoError "Failed to create directory $SECOND_DIR"
@@ -31,152 +35,55 @@ for i in {6..10};
 do
     echo "file$i">"$SECOND_DIR/file$i.txt" || bu echoError "Failed to create file$i.txt"
 done
-
-
-dagExportTest(){
-    local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
-    local GOT=$(go run $MAIN_DIR dag $ENTRY_DIR --export)
-    
-    if [[ $WANT == $GOT ]]; 
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] dagExportTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] dagExportTest\e[0m"; echo
-        exit 1
-    fi
-}
-
-pinTest(){
-    local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
-    local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR --key="$PINATA_API_JWT_TEST" | jq -r .hash)
-
-    if [[ $WANT == $GOT ]]; 
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] pinTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] pinTest\e[0m"; echo
-        exit 1
-    fi
-
-}
-
-deleteByHashTest(){
-    local WANT=true
-    local GOT=$(go run $MAIN_DIR delete bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4 --key="$PINATA_API_JWT_TEST" | jq .success)
-    
-    if [[ $GOT == $WANT ]];
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] deleteByHashTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] deleteByHashTest\e[0m"; echo
-        exit 1
-    fi
-}
-pinnedMeta(){
-    go run $MAIN_DIR pinned meta --key="$PINATA_API_JWT_TEST" | jq
-}
-pinnedHash(){
-    go run $MAIN_DIR pinned bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4 --key="$PINATA_API_JWT_TEST" | jq
-}
-
-pinWithMetaTest(){
-    local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
-
-    local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR meta --key="$PINATA_API_JWT_TEST"  | jq -r .hash)
-
-    if [[ $WANT == $GOT ]]; 
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] pinWithMetaTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] pinWithMetaTest\e[0m"; echo
-        exit 1
-    fi
-
-}
-
-deleteByMetaTest(){
-    local WANT=true
-    local GOT=$(go run $MAIN_DIR delete meta --key="$PINATA_API_JWT_TEST" | jq .success)
-    if [[ $GOT == $WANT ]];
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] deleteByMetaTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] deleteByMetaTest\e[0m"; echo
-        echo "SUCCESS: $GOT"
-        exit 1
-    fi
-}
-
-pinWithMetaForceTest(){
-    local WANT="bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4"
-    local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR foobar --key="$PINATA_API_JWT_TEST" | jq -r .hash)
-
-    if [[ $WANT == $GOT ]]; 
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] pinWithMetaForceTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] pinWithMetaForceTest\e[0m"; echo
-        exit 1
-    fi
-
-}
-pinWithMetaOverwriteTest(){
-    local WANT="OK"
-    local GOT=$(go run $MAIN_DIR pin $ENTRY_DIR foobar --overwrite --key="$PINATA_API_JWT_TEST")
-    if [[ $WANT == $GOT ]]; 
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] pinWithMetaOverwriteTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] pinWithMetaOverwriteTest\e[0m"; echo
-        exit 1
-    fi
-
-}
-
-
-deleteByMetaOverwriteTest(){
-    local WANT=true
-    local GOT=$(go run $MAIN_DIR delete foobar --key="$PINATA_API_JWT_TEST" | jq .success)
-
-    if [[ $GOT == $WANT ]];
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] deleteByMetaOverwriteTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] deleteByMetaOverwriteTest\e[0m"; echo
-        exit 1
-    fi
-}
-
-deleteByMetaForceTest(){
-    local WANT=true
-    local GOT=$(go run $MAIN_DIR delete foobar --key="$PINATA_API_JWT_TEST" | jq .success)
-
-    if [[ $GOT == $WANT ]];
-    then
-        echo -en "\033[1G\e[0m\e[36;1m[PASS] deleteByMetaOverwriteTest\e[0m"; echo
-    else
-        echo -en "\033[1G\e[0m\e[31;1m[FAIL] deleteByMetaOverwriteTest\e[0m"; echo
-        exit 1
-    fi
-}
 set -x
-bu echoInfo "Clearing failed results if any..."
 
+runTest() {
+  local test_cmd="$1"
+  local test_name="$2"
+
+
+  # Execute the test command and get the exit code
+  eval "$test_cmd"
+  exit_code=$?
+
+  # Get the command name
+  # Check the exit code and print the result
+  if [ $exit_code -eq 0 ]; then
+    echo "[PASS] Test $test_name"
+  else
+    echo "[FAIL] Test $test_name"
+  fi
+}
+
+
+# Clearing failed results
+bu echoInfo "Clearing failed results if any..."
 go run $MAIN_DIR delete bafybeiajf7mv3htewce3zozleukne3vfmagrc7bmk7uzzcsy7gjexkuwg4 --key="$PINATA_API_JWT_TEST" --verbose &> /dev/null ||:
 go run $MAIN_DIR delete meta --key="$PINATA_API_JWT_TEST" --verbose &> /dev/null ||:
 
 bu echoInfo "Running tests"
-set +x
-TESTS=(dagExportTest pinWithMetaTest pinnedMeta deleteByMetaTest pinTest pinnedHash deleteByHashTest pinWithMetaTest pinnedMeta pinWithMetaOverwriteTest pinnedMeta deleteByMetaOverwriteTest pinWithMetaTest pinnedMeta pinWithMetaForceTest pinnedMeta deleteByMetaForceTest)
-for TEST in "${TESTS[@]}"; do
-    set -x
-    $TEST
-    set +x
-done
 
-set -x
+runTest "go run $MAIN_DIR dag $ENTRY_DIR" "Dag"
+
+runTest "go run $MAIN_DIR pin $ENTRY_DIR --key=$PINATA_API_JWT_TEST" "Pin without metadata"
+runTest "go run $MAIN_DIR pinned $CID --key=$PINATA_API_JWT_TEST" "Pinned wiht CID"
+runTest "go run $MAIN_DIR delete $CID --key=$PINATA_API_JWT_TEST" "Unpin with CID"
+
+runTest "go run $MAIN_DIR pin $ENTRY_DIR $META --key=$PINATA_API_JWT_TEST" "Pin with metadata"
+runTest "go run $MAIN_DIR pinned $META --key=$PINATA_API_JWT_TEST" "Pinned with metadata"
+runTest "go run $MAIN_DIR delete $META --key=$PINATA_API_JWT_TEST" "Delete with metadata"
+
+runTest "go run $MAIN_DIR pin $ENTRY_DIR $META --key=$PINATA_API_JWT_TEST" "Pin with metadata"
+runTest "go run $MAIN_DIR pinned $META --key=$PINATA_API_JWT_TEST" "Pinned with metadata"
+runTest "go run $MAIN_DIR pin $ENTRY_DIR $META_FORCE --key=$PINATA_API_JWT_TEST --force=true" "Forced pin"
+runTest "go run $MAIN_DIR delete $META_FORCE --key=$PINATA_API_JWT_TEST" "Delete with metadata"
+
+runTest "go run $MAIN_DIR pin $ENTRY_DIR $META --key=$PINATA_API_JWT_TEST" "Pin with metadata"
+runTest "go run $MAIN_DIR pinned $META --key=$PINATA_API_JWT_TEST" "Pinned with metadata"
+runTest "go run $MAIN_DIR pin $ENTRY_DIR $META_FORCE --key=$PINATA_API_JWT_TEST --overwrite=true" "Overwrite pin"
+runTest "go run $MAIN_DIR delete $META_FORCE --key=$PINATA_API_JWT_TEST" "Delete with metadata"
+
+
 bu echoInfo "All tests finished. Cleaning up the environment..."
-
 rm -rf $ENTRY_DIR || bu echoError "Failed to clean up an the environment"
-
 bu echoInfo "All done..."
